a5873e01a3214a64bd4044350af8458e310280c0
pbonebright <patrick.bonebright@gmail.com>
2016-09-09 11:36:03 -0600

    Create ModelInfoTemplate.js

---
+++ js/ModelInfoTemplate.js
@@ -0,0 +1,19 @@
+$document.ready( function() {
+  if $.urlParam('modelID') != '' {
+    $.get( "http://174.27.114.24/gmlc-demo/getModelInfo.cgi?" + $.urlParam('modelID'), function( data ) {
+      $(#modelName).html(data.modelName);  
+    });        
+  }
+};
+
+$.urlParam = function(name) {
+  var results = new RegExp('[\?&]' + modelID + '=([^&#]*)').exec(window.location.href);
+  if (results==null){
+    return null;
+  }
+  else{
+    return results[1] || 0;
+  }
+}
+
+
